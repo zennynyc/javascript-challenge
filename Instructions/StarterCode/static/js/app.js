64d572be5ec37f4cfda76fba54a7cd5052f8609a
zennynyc <61849993+zennynyc@users.noreply.github.com>
2020-06-25 18:47:54 -0400

    commit

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,31 @@
 // from data.js
 var tableData = data;
+console.log(data);
 
-// YOUR CODE HERE!
+var tbody=d3.select("tbody");
+// loop and access each object in the table
+tableData.forEach(addRow);
+
+function addRow(item) {
+    var row=tbody.append("tr");
+    var row_vals=Object.values(item);
+
+
+    row_vals.forEach(data=>{
+        row.append('td').text(data);
+    })
+}
+
+var button_click = d3.select('#filter-btn');
+button_click.on('click', function(){
+    tbody.html("");
+
+    var formatedDate = d3.select('#datetime').property('value');
+    console.log(formatedDate);
+
+    var input_date = tableData.filter( x => x.datetime === formatedDate);
+    console.log(input_date);
+
+    input_date.forEach(addRow);
+
+});
